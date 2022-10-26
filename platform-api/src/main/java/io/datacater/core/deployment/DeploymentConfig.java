b3d1c86483b4fdcf624e553d8661387b8eb7c61c
ChrisRousey <104754971+ChrisRousey@users.noreply.github.com>
2022-10-26 11:39:26 +0200

    feat(deployment): removed unused class
    refs: #13, #4

--- platform-api/src/main/java/io/datacater/core/deployment/DeploymentConfig.java
+++
@@ -1,9 +0,0 @@
-package io.datacater.core.deployment;
-
-public record DeploymentConfig(
-    String streamInTopic,
-    String streamInDeserializerClass,
-    String streamInBootstrapServer,
-    String streamOutTopic,
-    String streamOutDeserializerClass,
-    String streamOutBootstrapServer) {}
