2ec37d76965210c13ab053c5f06853c89ebb8a4d
ChrisRousey <104754971+ChrisRousey@users.noreply.github.com>
2022-11-01 11:16:42 +0100

    feat(deployment): removed unused methods from deploymentspec
    refs: #4, #13

--- platform-api/src/main/java/io/datacater/core/deployment/DeploymentSpec.java
+++ platform-api/src/main/java/io/datacater/core/deployment/DeploymentSpec.java
@@ -1,10 +1,5 @@
 package io.datacater.core.deployment;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
@@ -16,16 +11,4 @@ public record DeploymentSpec(
             "pipeline": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
           }
         """)
-        Map<String, String> deployment) {
-  public static JsonNode serializeDeploymentSpec(Map<String, String> deployment)
-      throws JsonProcessingException {
-    ObjectMapper objectMapper = new ObjectMapper();
-    DeploymentSpec ds = new DeploymentSpec(deployment);
-    return objectMapper.readTree(objectMapper.writeValueAsString(ds));
-  }
-
-  @JsonCreator
-  static DeploymentSpec from(@JsonProperty("deployment") Map<String, String> deployment) {
-    return new DeploymentSpec(deployment);
-  }
-}
+        Map<String, String> deployment) {}
