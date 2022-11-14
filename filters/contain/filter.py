cea71d079e65da0d85ed547f5b2a6dcd09840d9f
Stefan Sprenger <stefan@datacater.io>
2022-11-14 16:52:43 +0100

    chore(filters): Clean up print statements

--- filters/contain/filter.py
+++ filters/contain/filter.py
@@ -1,3 +1,2 @@
 def filter(value, row: dict, config: dict) -> bool:
-    print("contain")
     return config["value"] in value
--- filters/empty/filter.py
+++ filters/empty/filter.py
@@ -1,3 +1,2 @@
 def filter(value, row: dict, config: dict) -> bool:
-    print("empty")
     return value is None
