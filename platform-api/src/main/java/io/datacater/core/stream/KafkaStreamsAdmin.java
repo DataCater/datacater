c7b1c47672d3ff8e81c75ae8a284ba33161ff2e4
Stefan Sprenger <stefan@datacater.io>
2022-12-05 04:53:11 +0100

    feat(streams): Do not require JSON objects in keys/values

--- platform-api/src/main/java/io/datacater/core/stream/KafkaStreamsAdmin.java
+++ platform-api/src/main/java/io/datacater/core/stream/KafkaStreamsAdmin.java
@@ -238,14 +238,7 @@ public class KafkaStreamsAdmin implements StreamService {
     metadata.put("partition", item.partition());
     metadata.put("offset", item.offset());
     metadata.put("timestamp", item.timestamp());
-    return new StreamMessage(objectToMap(item.key()), objectToMap(item.value()), metadata);
-  }
-
-  private Map<String, Object> objectToMap(Object obj) {
-    if (obj == null) {
-      return new JsonObject().getMap();
-    }
-    return new JsonObject(obj.toString()).getMap();
+    return new StreamMessage(item.key(), item.value(), metadata);
   }
 
   public CompletableFuture<Void> deleteStream() {
