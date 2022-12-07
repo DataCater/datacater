5205809e97ae6dd8a4b62638ab32603767a8f2ba
Stefan Sprenger <stefan@datacater.io>
2022-12-07 16:18:42 +0100

    feat(api): Update version to v1

--- ui/src/helpers/getApiPathPrefix.js
+++ ui/src/helpers/getApiPathPrefix.js
@@ -1,6 +1,6 @@
 export function getApiPathPrefix(includeHostname = false) {
   const hostname = window.location.protocol + "//" + window.location.host;
-  const pathPrefix = "/api/alpha";
+  const pathPrefix = "/api/v1";
 
   if (includeHostname) {
     return hostname + pathPrefix;
