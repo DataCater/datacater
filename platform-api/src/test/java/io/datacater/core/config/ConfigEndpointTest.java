7001b204c1822e9840bd4c2d8a6fc39d30b4ac74
olis1996 <oli@datacater.io>
2022-12-09 16:25:18 +0100

    feat(config): implemented test cases for put and delete, improved existing tests

--- platform-api/src/test/java/io/datacater/core/config/ConfigEndpointTest.java
+++ platform-api/src/test/java/io/datacater/core/config/ConfigEndpointTest.java
@@ -1,6 +1,7 @@
 package io.datacater.core.config;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -10,13 +11,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
-import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.*;
 
@@ -28,71 +26,181 @@ class ConfigEndpointTest {
   JsonNode postFirstConfigJson;
   JsonNode postSecondConfigJson;
   JsonNode postThirdConfigJson;
-
-  List<ConfigEntity> configEntities = new ArrayList<>();
+  JsonNode postConfigMissingNameJson;
+  JsonNode postConfigUnnecessaryPropertyJson;
 
   @BeforeAll
   public void setUp() throws IOException {
     postFirstConfigJson = getJsonFromFile("configTestFiles/post/post-config-test1-valid.json");
     postSecondConfigJson = getJsonFromFile("configTestFiles/post/post-config-test2-valid.json");
     postThirdConfigJson = getJsonFromFile("configTestFiles/post/post-config-test3-valid.json");
+    postConfigMissingNameJson =
+        getJsonFromFile("configTestFiles/post/post-config-test-missing-name.json");
+    postConfigUnnecessaryPropertyJson =
+        getJsonFromFile("configTestFiles/post/post-config-test-unnecessary-property.json");
   }
-
-  /*
-  @Test
-  void getAllConfigs() {
-    given().get().then().statusCode(200);
-  }
-   */
 
   @Test
   @Order(1)
   void testGetEmptyConfigList() {
     // get an empty list of configs
-    Response response = given().get();
-    Assertions.assertEquals(200, response.statusCode());
-    Assertions.assertEquals("[]", response.getBody().asString());
+    given().get().then().statusCode(200).and().body(containsString("[]"));
   }
 
   @Test
   @Order(2)
   void testPostConfigs() throws JsonProcessingException {
     // create three configs
-    RequestSpecification request = RestAssured.given();
-    ObjectMapper objectMapper = new ObjectMapper();
-
     for (var jsonNode :
         new JsonNode[] {postFirstConfigJson, postSecondConfigJson, postThirdConfigJson}) {
-      String json = jsonNode.toString();
 
-      request.header("Content-Type", "application/json");
-      request.body(json);
-
-      Response response = request.post();
-      Assertions.assertEquals(200, response.getStatusCode());
-      configEntities.add(objectMapper.readValue(response.getBody().asString(), ConfigEntity.class));
+      given()
+          .header("Content-Type", "application/json")
+          .body(jsonNode.toString())
+          .post()
+          .then()
+          .statusCode(200);
     }
   }
 
   @Test
   @Order(3)
-  void testGetConfigFail() {
-    // try to get a non-existing config
-    given().pathParam("uuid", UUID.randomUUID()).get("/{uuid}").then().statusCode(404);
+  void testPostFailMissingProperties() {
+    // request with body that is missing properties inside json
+    given()
+        .header("Content-Type", "application/json")
+        .body(postConfigMissingNameJson.toString())
+        .post()
+        .then()
+        .statusCode(400);
   }
 
   @Test
   @Order(4)
+  void testPostFailUnnecessaryProperties() {
+    // post with body that contains unnecessary properties
+    // TODO change to return 400 if body contains properties which are not specified
+    given()
+        .header("Content-Type", "application/json")
+        .body(postConfigUnnecessaryPropertyJson.toString())
+        .post()
+        .then()
+        .statusCode(200);
+  }
+
+  @Test
+  @Order(5)
+  void testGetConfigFail() {
+    // get a non-existing config
+    given().pathParam("uuid", UUID.randomUUID()).get("/{uuid}").then().statusCode(404);
+  }
+
+  @Test
+  @Order(6)
   void testGetConfigList() throws JsonProcessingException {
     // get all configs
     Response response = given().get();
-    Assertions.assertEquals(200, response.statusCode());
 
     ObjectMapper objectMapper = new ObjectMapper();
 
     ArrayList<ConfigEntity> receivedConfigs =
         objectMapper.readValue(
             response.getBody().asString(), new TypeReference<ArrayList<ConfigEntity>>() {});
+
+    Assertions.assertEquals(200, response.statusCode());
+    // TODO change to "3" when changing response for post with unnecessary properties inside body
+    Assertions.assertEquals(4, receivedConfigs.size());
+  }
+
+  @Test
+  @Order(7)
+  void testDeleteConfigFail() {
+    // delete a non-existing config
+    given().pathParam("uuid", UUID.randomUUID()).delete("/{uuid}").then().statusCode(404);
+  }
+
+  @Test
+  @Order(8)
+  void testDeleteConfig() throws JsonProcessingException {
+    // deletes an existing config
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String responseJson = given().get().getBody().asString();
+    UUID configId =
+        objectMapper
+            .readValue(responseJson, new TypeReference<ArrayList<ConfigEntity>>() {})
+            .get(0)
+            .getId();
+
+    given().pathParam("uuid", configId).delete("/{uuid}").then().statusCode(200);
+    given().pathParam("uuid", configId).get("/{uuid}").then().statusCode(404);
+  }
+
+  @Test
+  @Order(9)
+  void testPut() throws JsonProcessingException {
+    // updates an existing config
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String responseJson = given().get().getBody().asString();
+    UUID configId =
+        objectMapper
+            .readValue(responseJson, new TypeReference<ArrayList<ConfigEntity>>() {})
+            .get(0)
+            .getId();
+
+    given()
+        .header("Content-Type", "application/json")
+        .pathParam("uuid", configId)
+        .body(postThirdConfigJson)
+        .put("/{uuid}")
+        .then()
+        .statusCode(200);
+  }
+
+  @Test
+  @Order(10)
+  void testPutFailMissingProperties() throws JsonProcessingException {
+    // updates an existing config with missing properties inside the request body
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String responseJson = given().get().getBody().asString();
+    UUID configId =
+        objectMapper
+            .readValue(responseJson, new TypeReference<ArrayList<ConfigEntity>>() {})
+            .get(0)
+            .getId();
+
+    given()
+        .header("Content-Type", "application/json")
+        .pathParam("uuid", configId)
+        .body(postConfigMissingNameJson.toString())
+        .put("/{uuid}")
+        .then()
+        .statusCode(400);
+  }
+
+  @Test
+  @Order(11)
+  void testPutFailUnnecessaryProperties() throws JsonProcessingException {
+    // updates an existing config with unnecessary properties inside the request body
+    // TODO change to return 400 if body contains properties which are not specified
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    String responseJson = given().get().getBody().asString();
+    UUID configId =
+        objectMapper
+            .readValue(responseJson, new TypeReference<ArrayList<ConfigEntity>>() {})
+            .get(0)
+            .getId();
+
+    given()
+        .header("Content-Type", "application/json")
+        .body(postConfigUnnecessaryPropertyJson.toString())
+        .pathParam("uuid", configId)
+        .put("/{uuid}")
+        .then()
+        .statusCode(200);
   }
 
   private JsonNode getJsonFromFile(String path) throws IOException {
