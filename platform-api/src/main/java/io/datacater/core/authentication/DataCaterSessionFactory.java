c00f59649bcbfd1f215e4a098a2912ae2e8f404d
ChrisRousey <104754971+ChrisRousey@users.noreply.github.com>
2022-12-21 13:29:04 +0100

    feat(runtime-auth-switch): revert changes to DCSessionFactory
    refs: #95

--- platform-api/src/main/java/io/datacater/core/authentication/DataCaterSessionFactory.java
+++ platform-api/src/main/java/io/datacater/core/authentication/DataCaterSessionFactory.java
@@ -1,7 +1,6 @@
 package io.datacater.core.authentication;
 
 import io.quarkus.security.UnauthorizedException;
-import io.quarkus.security.identity.CurrentIdentityAssociation;
 import io.quarkus.security.identity.SecurityIdentity;
 import io.smallrye.mutiny.Uni;
 import java.util.function.BiFunction;
@@ -21,59 +20,35 @@ public class DataCaterSessionFactory {
 
   @Inject Mutiny.SessionFactory sf;
 
-  @Inject CurrentIdentityAssociation si;
+  @Inject SecurityIdentity si;
 
-  private Uni<SecurityIdentity> getPrincipal() {
-    return si.getDeferredIdentity();
+  private String getPrincipal() {
+    String principalName = si.getPrincipal().getName();
+    if (principalName == null || principalName.isEmpty()) {
+      throw new UnauthorizedException();
+    }
+
+    LOGGER.info(String.format("Detected principal with name := %s.", principalName));
+    return principalName;
   }
 
   public <T> Uni<T> withSession(Function<Session, Uni<T>> work) {
-    return getPrincipal()
-        .onItem()
-        .ifNull()
-        .failWith(new UnauthorizedException())
-        .onItem()
-        .ifNotNull()
-        .transformToUni(
-            x ->
-                sf.withSession(
-                    session -> {
-                      LOGGER.info(
-                          String.format(
-                              "Detected principal with name := %s.", x.getPrincipal().getName()));
-                      var update =
-                          session
-                              .createNativeQuery(
-                                  String.format(
-                                      "SET datacater.tenant = %s", x.getPrincipal().getName()))
-                              .executeUpdate();
-                      return update.onItem().transformToUni(ignore -> work.apply(session));
-                    }));
+    String nativeQuery = String.format("SET datacater.tenant = %s", getPrincipal());
+
+    return sf.withSession(
+        session -> {
+          var update = session.createNativeQuery(nativeQuery).executeUpdate();
+          return update.onItem().transformToUni(ignore -> work.apply(session));
+        });
   }
 
   public <T> Uni<T> withTransaction(BiFunction<Session, Transaction, Uni<T>> work) {
-    return getPrincipal()
-        .onItem()
-        .ifNull()
-        .failWith(new UnauthorizedException())
-        .onItem()
-        .ifNotNull()
-        .transformToUni(
-            x ->
-                sf.withTransaction(
-                    (session, transaction) -> {
-                      LOGGER.info(
-                          String.format(
-                              "Detected principal with name := %s.", x.getPrincipal().getName()));
-                      var update =
-                          session
-                              .createNativeQuery(
-                                  String.format(
-                                      "SET datacater.tenant = %s", x.getPrincipal().getName()))
-                              .executeUpdate();
-                      return update
-                          .onItem()
-                          .transformToUni(ignore -> work.apply(session, transaction));
-                    }));
+    String nativeQuery = String.format("SET datacater.tenant = %s", getPrincipal());
+
+    return sf.withTransaction(
+        (session, transaction) -> {
+          var update = session.createNativeQuery(nativeQuery).executeUpdate();
+          return update.onItem().transformToUni(ignore -> work.apply(session, transaction));
+        });
   }
 }
