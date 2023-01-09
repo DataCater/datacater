95403f500b2caa2904cdaac280e649fe2928f604
Chris <104754971+ChrisRousey@users.noreply.github.com>
2023-01-09 13:42:58 +0100

    feat(python-runner): remove python-runner pool creation from the app (#115)
    * feat(python-runner): remove python-runner pool creation from the application startup
    
    refs: #67
    
    * feat(pythonrunner-pool): fixed missing import error
    
    refs: #67

--- platform-api/src/main/java/io/datacater/core/kubernetes/DataCaterK8sConfig.java
+++ platform-api/src/main/java/io/datacater/core/kubernetes/DataCaterK8sConfig.java
@@ -16,25 +16,6 @@ public class DataCaterK8sConfig {
   static final String NAME = "python-runner";
 
   static final Map<String, String> LABELS = Map.of("app.kubernetes.io/name", NAME);
-  static final String IMAGE_NAME =
-      ConfigProvider.getConfig().getValue("datacater.pythonrunner.image.name", String.class);
-  static final String IMAGE_TAG =
-      ConfigProvider.getConfig()
-          .getOptionalValue("datacater.pythonrunner.image.version", String.class)
-          .orElse("alpha-20221117");
-  static final Optional<String> IMAGE_PULL_SECRET =
-      ConfigProvider.getConfig()
-          .getOptionalValue("datacater.pythonrunner.images.imagePullSecret", String.class);
-  static final int CONTAINER_PORT =
-      ConfigProvider.getConfig()
-          .getOptionalValue("datacater.pythonrunner.image.containerPort", Integer.class)
-          .orElse(50000);
-
-  static final String CONTAINER_PORT_NAME =
-      ConfigProvider.getConfig()
-          .getOptionalValue("datacater.pythonrunner.image.containerPortName", String.class)
-          .orElse("http");
-
   static final String PYTHON_RUNNER_SERVICE_NAME =
       ConfigProvider.getConfig()
           .getOptionalValue("datacater.pythonrunner.serviceName", String.class)
