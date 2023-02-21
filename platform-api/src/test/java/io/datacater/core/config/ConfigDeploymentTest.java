06bb29fccb57a88f7d1ad1367a0f472da9ecbb08
ChrisRousey <104754971+ChrisRousey@users.noreply.github.com>
2023-02-21 19:15:45 +0100

    feat(config-mapping): added deployment tests
    -failing at the time of commitment. The way we get deployments inside DeplyomentEndpoint needs to be updated, since the pipeline and stream can be inside the deployment config

---
+++ platform-api/src/test/java/io/datacater/core/config/ConfigDeploymentTest.java
@@ -0,0 +1,150 @@
+package io.datacater.core.config;
+
+import static io.restassured.RestAssured.given;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import io.datacater.core.pipeline.PipelineEntity;
+import io.datacater.core.stream.StreamEntity;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.Response;
+import java.io.IOException;
+import java.net.URL;
+import java.util.UUID;
+import org.jboss.logging.Logger;
+import org.junit.jupiter.api.*;
+
+@QuarkusTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ConfigDeploymentTest {
+  private static final Logger LOGGER = Logger.getLogger(ConfigStreamTest.class);
+
+  JsonNode configJson;
+  JsonNode streamInJson;
+  JsonNode streamOutJson;
+  JsonNode pipelineJson;
+  JsonNode deploymentJson;
+  UUID configUUID;
+  UUID pipelineUUID;
+  final String baseURI = "http://localhost:8081";
+  final String deploymentsPath = "/deployments";
+  final String streamsPath = "/streams";
+  final String pipelinesPath = "/pipelines";
+  final String configsPath = "/configs";
+  String configUUIDPlaceholder = "configUUIDPlaceholder";
+  String streaminUUIDPlaceholder = "streaminUUIDPlaceholder";
+  String streamoutUUIDPlaceholder = "streamoutUUIDPlaceholder";
+  String pipelineUUIDPlaceholder = "pipelineUUIDPlaceholder";
+
+  @BeforeAll
+  public void setUp() throws IOException {
+    ObjectMapper mapper = new JsonMapper();
+
+    URL configURL =
+        ClassLoader.getSystemClassLoader().getResource("configTestFiles/deployments/config.json");
+    URL streamInURL =
+        ClassLoader.getSystemClassLoader()
+            .getResource("configTestFiles/deployments/stream-in.json");
+    URL streamOutURL =
+        ClassLoader.getSystemClassLoader()
+            .getResource("configTestFiles/deployments/stream-out.json");
+    URL pipelineURL =
+        ClassLoader.getSystemClassLoader().getResource("configTestFiles/deployments/pipeline.json");
+    URL deploymentURL =
+        ClassLoader.getSystemClassLoader()
+            .getResource("configTestFiles/deployments/deployment.json");
+
+    configJson = mapper.readTree(configURL);
+    streamInJson = mapper.readTree(streamInURL);
+    streamOutJson = mapper.readTree(streamOutURL);
+    pipelineJson = mapper.readTree(pipelineURL);
+    deploymentJson = mapper.readTree(deploymentURL);
+  }
+
+  @Test
+  @Order(1)
+  void postResources() throws JsonProcessingException {
+    ObjectMapper mapper = new JsonMapper();
+
+    LOGGER.info(streamInJson.toString());
+    LOGGER.info(streamOutJson.toString());
+
+    Response streamInResponse =
+        given()
+            .header("Content-Type", "application/json")
+            .body(streamInJson.toString())
+            .baseUri(baseURI)
+            .post(streamsPath);
+
+    Response streamOutResponse =
+        given()
+            .header("Content-Type", "application/json")
+            .body(streamOutJson.toString())
+            .baseUri(baseURI)
+            .post(streamsPath);
+
+    StreamEntity streamIn =
+        mapper.readValue(streamInResponse.body().asString(), StreamEntity.class);
+    StreamEntity streamOut =
+        mapper.readValue(streamOutResponse.body().asString(), StreamEntity.class);
+
+    String pipelineString = pipelineJson.toString();
+    pipelineString = pipelineString.replace(streaminUUIDPlaceholder, streamIn.getId().toString());
+    pipelineString = pipelineString.replace(streamoutUUIDPlaceholder, streamOut.getId().toString());
+
+    Response pipelineResponse =
+        given()
+            .header("Content-Type", "application/json")
+            .body(pipelineString)
+            .baseUri(baseURI)
+            .post(pipelinesPath);
+
+    PipelineEntity pipeline =
+        mapper.readValue(pipelineResponse.body().asString(), PipelineEntity.class);
+    pipelineUUID = pipeline.getId();
+
+    Assertions.assertEquals(200, pipelineResponse.getStatusCode());
+  }
+
+  @Test
+  @Order(2)
+  void postConfig() throws JsonProcessingException {
+    ObjectMapper mapper = new JsonMapper();
+    String configString = configJson.toString();
+    configString = configString.replace(pipelineUUIDPlaceholder, pipelineUUID.toString());
+
+    Response configResponse =
+        given()
+            .header("Content-Type", "application/json")
+            .body(configString)
+            .baseUri(baseURI)
+            .post(configsPath);
+
+    ConfigEntity ce = mapper.readValue(configResponse.body().asString(), ConfigEntity.class);
+
+    configUUID = ce.getId();
+
+    Assertions.assertEquals(200, configResponse.getStatusCode());
+  }
+
+  @Test
+  @Order(3)
+  void postDeployment() {
+    String deploymentString = deploymentJson.toString();
+    deploymentString = deploymentString.replace(configUUIDPlaceholder, configUUID.toString());
+    LOGGER.info(deploymentString);
+    Response deploymentResponse =
+        given()
+            .header("Content-Type", "application/json")
+            .body(deploymentString)
+            .baseUri(baseURI)
+            .post(deploymentsPath);
+
+    LOGGER.info(deploymentResponse.body().asString());
+
+    Assertions.assertEquals(200, deploymentResponse.getStatusCode());
+  }
+}
