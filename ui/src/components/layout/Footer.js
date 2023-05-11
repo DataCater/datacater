42c6d7acf89cbff1921aa29079fa5f6d36a2fd5c
ChrisRousey <104754971+ChrisRousey@users.noreply.github.com>
2023-05-11 09:44:12 +0200

    ui(info): updated footer with links for respective categories

--- ui/src/components/layout/Footer.js
+++ ui/src/components/layout/Footer.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchInfo } from "../../actions/info";
+import {
+  Info,
+  BookOpen,
+  User,
+} from "react-feather";
+import "../../scss/nav.scss";
 
 
 class Footer extends Component {
@@ -17,6 +24,26 @@ class Footer extends Component {
     };
   }
 
+    renderNavItem(label, href, icon) {
+      let classNames = "nav-link d-flex align-items-center";
+      classNames = "nav-item";
+      if (
+        window.location.pathname !== undefined &&
+        window.location.pathname.includes(href)
+      ) {
+        classNames += " active";
+      }
+
+      return (
+        <ul className={classNames}>
+          <Link className="nav-link text-black" to={href} role="button">
+            {icon}
+            {label}
+          </Link>
+        </ul>
+      );
+    }
+
   componentDidMount() {
     this.props
       .fetchInfo()
@@ -31,22 +58,26 @@ class Footer extends Component {
       }
 
     return (
-        <div>
-          <div>
-              <p>version info:</p>
-              <p>version: {info.version.version}</p>
-              <p>base image: {info.version.baseImage}</p>
-              <p>pipeline image: {info.version.pipelineImage}</p>
-              <p>python-runner image: {info.version.pythonRunnerImage}</p>
-          </div>
-          <div>
-              <p>documentation:</p>
-              <a href="{info.resources.streams.documentationUrl}">streams</a>
-              <a href="{info.resources.deployments.documentationUrl}">deployments</a>
-              <a href="{info.resources.pipelines.documentationUrl}">pipelines</a>
-              <a href="{info.resources.configs.documentationUrl}">configs</a>
-          </div>
-      </div>
+          <nav
+            id="bottomnav"
+            className="navbar navbar-expand-lg navbar-light justify-content-center"
+          >
+          {this.renderNavItem(
+            "Version",
+            "/",
+            <Info className="feather-icon me-2" />
+          )}
+          {this.renderNavItem(
+            "Documentation",
+            "/",
+            <BookOpen className="feather-icon me-2" />
+          )}
+          {this.renderNavItem(
+            "Contact",
+            "/",
+            <User className="feather-icon me-2" />
+          )}
+ </nav>
     );
   }
 }
@@ -62,3 +93,26 @@ const mapDispatchToProps = {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Footer);
+/*
+       <div>
+          <div>
+              <p>version info:</p>
+              <p>version: {info.version.version}</p>
+              <p>base image: {info.version.baseImage}</p>
+              <p>pipeline image: {info.version.pipelineImage}</p>
+              <p>python-runner image: {info.version.pythonRunnerImage}</p>
+          </div>
+          <div>
+              <p>documentation:</p>
+              <p><a href="{info.resources.streams.documentationUrl}">streams</a></p>
+              <p><a href="{info.resources.deployments.documentationUrl}">deployments</a></p>
+              <p><a href="{info.resources.pipelines.documentationUrl}">pipelines</a></p>
+              <p><a href="{info.resources.configs.documentationUrl}">configs</a></p>
+          </div>
+          <div>
+                <p>contact:</p>
+                <p>name: {info.contact.name}</p>
+                <p>email: {info.contact.email}</p>
+                <p>url: {info.contact.url}</p>
+            </div>
+      </div> */
