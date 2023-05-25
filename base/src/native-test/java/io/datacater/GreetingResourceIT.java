94fe2ced9d7e823fcbed61c7f47f3f15f93d7c17
ChrisRousey <104754971+ChrisRousey@users.noreply.github.com>
2023-05-25 09:14:41 +0200

    initial app commit

---
+++ base/src/native-test/java/io/datacater/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.datacater;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
