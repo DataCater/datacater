9561e295b36f36219b6ec778ca39bf39a3a532e0
ChrisRousey <104754971+ChrisRousey@users.noreply.github.com>
2023-08-08 09:47:41 +0200

    feat(project): add plus icon to `add project` option
    refs: #235

--- ui/src/components/layout/Navigation.js
+++ ui/src/components/layout/Navigation.js
@@ -14,6 +14,7 @@ import {
   Mail,
   LogOut,
   PlayCircle,
+  PlusCircle,
   Wind,
   Tool,
   Info,
@@ -86,6 +87,11 @@ class Navigation extends Component {
               <small>{project.name}</small>
             </Dropdown.Item>
           ))}
+        <Dropdown.Item eventKey="create">
+          <small>
+            <PlusCircle className="feather-icon" /> Create Project
+          </small>
+        </Dropdown.Item>
       </DropdownButton>
     );
   }
